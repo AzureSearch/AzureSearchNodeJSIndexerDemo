ed1eee37b9e9009cb81f25c1bf16387759516d18
Jakub Jedryszek <jjed@microsoft.com>
2018-01-17 10:48:36 -0800

    remove files

--- js/UI/app.js
+++
@@ -1,8 +0,0 @@
-var React = require('react');
-var SearchUI = require('./components/SearchUI');
-var Config = require('../../config');
-
-React.render(
-	<SearchUI url={Config.serviceURL} apikey={Config.queryKey} index={Config.indexName}/>,
-	 document.body
-); 
